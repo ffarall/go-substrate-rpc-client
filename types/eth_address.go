96f5295ef00c5e57e6b57c87499888e0551130a1
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2025-05-08 17:47:46 -0300

    feat: :sparkles: Add `EthAddress` and `EthSignature`

---
+++ types/eth_address.go
@@ -0,0 +1,46 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/snowfork/go-substrate-rpc-client/v4/scale"
+)
+
+type EthAddress [20]byte
+
+func NewEthAddress(str string) (EthAddress, error) {
+	b, err := HexDecodeString(str)
+	if err != nil {
+		return EthAddress{}, err
+	}
+	var address [20]byte
+	copy(address[:], b)
+
+	return address, nil
+}
+
+func (m EthAddress) Encode(encoder scale.Encoder) error {
+	var err error
+	err = encoder.Encode(m)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *EthAddress) Decode(decoder scale.Decoder) error {
+	// If the length is 20, it's an EthAddress
+	var encodedBytes []byte
+	err := decoder.Decode(&encodedBytes)
+	if err != nil {
+		return err
+	}
+
+	if len(encodedBytes) == 20 {
+		copy(m[:], encodedBytes)
+		return nil
+	} else {
+		return fmt.Errorf("Invalid length for EthAddress (should be 20 bytes): %v", len(encodedBytes))
+	}
+}
